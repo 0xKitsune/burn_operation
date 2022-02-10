d705fd33e3da33ae60fb2e73a4fede5b50689fa0
machine <0xKitsune@protonmail.com>
2022-02-10 00:29:56 -0500

    added rayon for parallel deletion, going to add logic to zero instead of just delete, and zero in parallel to wipe faster than standard os wipes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::fs;
+extern crate glob;
+use glob::glob;
+use std::time::{Instant};
+use rayon::prelude::*;
+
+
 fn main() {
-    println!("Hello, world!");
+        let now = Instant::now();
+
+       println!("Time elapsed: {} seconds", now.elapsed().as_secs());
+
+    }
+
+
+    
+fn burn_operation(){
+
+    let all_files = glob("/**").expect("Failed to read glob pattern");
+
+    let file_failures: Vec<_> = all_files.par_iter().map(|path| {
+
+    });
+    
+
+}
+
+
+fn burn_file(path: &str) -> std::io::Result<()> {
+    fs::remove_file(path)?;
+    Ok(())
 }
