a7cf13db96f3449ef281ab2b03fb0b8727b391a7
0xKitsune <0xKitsune@protonmail.com>
2022-08-27 12:51:24 -0400

    removed the authorize option, adding instructions to give permissions to burn_operation in readme

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,11 @@ use std::process::Command;
     about = "Securely wipe a computer at the speed of light."
 )]
 struct Args {
-    #[clap(
-        short,
-        long,
-        parse(from_flag),
-        help = "Authorize burn_operation to run with root privledges. This flag only needs to be run once. If this flag is not used before running `burn_operation`, root access will need to be granted with sudo or equivalent."
-    )]
-    pub authorize: bool,
 
     #[clap(
         short,
         long,
-        help = "Path to file or directory to wipe. If no argument is included, the default path is `/` which will wipe the entire computer.",
+        help = "Path to file or directory to wipe. If no argument is provided, the entire computer will be wiped.",
         default_value = "/"
     )]
     pub path: PathBuf,
@@ -46,9 +39,9 @@ struct Args {
 
 fn main() -> std::io::Result<()> {
     let args = Args::parse();
-    if args.authorize {
-        authorize()?;
-    } else if args.dead_mans_switch {
+
+
+ if args.dead_mans_switch {
         //read in the dead man's switch toml file
     } else {
         //burn everything from starting from root
@@ -58,24 +51,3 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-fn authorize() -> Result<(), Error> {
-    let operating_system = env::consts::OS;
-    let mut bin_path = home::cargo_home()?;
-    bin_path.push("bin");
-
-    match operating_system {
-        "linux" => {
-            Command::new("chmod")
-                .current_dir(bin_path)
-                .arg("666")
-                .arg("burn_operation")
-                .spawn()?;
-        }
-        "mac" => {}
-        "windows" => {}
-
-        _ => (),
-    }
-
-    Ok(())
-}
